d8b41c26e41c66acb7586b7870347bf98e5b5b4d
amalyshev <q3pp@yandex.ru>
2020-05-01 15:22:11 +0200

    removed factories folder

--- player.py
+++
@@ -1,9 +0,0 @@
-from player import Player
-
-class PlayerFactory:
-    @classmethod
-    def from_tg_update(cls, update):
-        player_id = update.message.from_user.id
-        player_name = update.message.from_user.first_name or update.message.from_user.username
-        return Player(player_id, player_name)
-
